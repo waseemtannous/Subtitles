eb8edbe1f80fb9c3b3f6b936802099e454aebaae
Waseem Tannous <waseemt99@gmail.com>
2024-06-24 14:28:04 +0300

    support multiple videos and languages

--- main.py
+++ main.py
@@ -7,13 +7,17 @@ from dotenv import dotenv_values
 
 # load the environment variables
 config = dotenv_values('.env')
-VIDEO_PATH = config['VIDEO_PATH']
-AUDIO_PATH = config['AUDIO_PATH']
-SRT_PATH = config['SRT_PATH']
-FINAL_OUTPUT_PATH = config['FINAL_OUTPUT_PATH']
-TARGET_LANGUAGE = config['TARGET_LANGUAGE']
-WHISPER_MODEL_SIZE = config['WHISPER_MODEL_SIZE']
+VIDEOS_PATH = config['VIDEOS_PATH']
+OUTPUT_PATH = config['OUTPUT_PATH']
 TRANSLATE = config['TRANSLATE'] == 'True'
+LANGUAGE_CODES = config['LANGUAGE_CODES'].split(",")
+ORIGINAL_SUBTITLES = config['ORIGINAL_SUBTITLES'] == 'True'
+WHISPER_MODEL_SIZE = config['WHISPER_MODEL_SIZE']
+
+# load the whisper model
+MODEL = whisper.load_model(name=WHISPER_MODEL_SIZE, download_root='whisper_models').to('cuda')
+
+RTL_LANGUAGES = ['ar', 'iw']
 
 # extracts the audio from a video file and saves it to the specified path.
 def extract_audio_from_video(video_path, audio_path):
@@ -38,19 +42,23 @@ def format_srt_time(seconds):
     return f"{hours:02}:{minutes:02}:{seconds:02},{milliseconds:03}"
 
 # save as srt file
-def save_as_srt(segments, file_name):
+def save_as_srt(segments, file_path, rtl=False):
     # check if a file exists with the same name as the file_name and delete it if it does
-    if os.path.exists(file_name):
-        os.remove(file_name)
+    if os.path.exists(file_path):
+        os.remove(file_path)
 
-    with open(file_name, "w") as file:
+    with open(file_path, "w") as file:
         for i, segment in enumerate(segments):
             start_time = format_srt_time(segment['start'])
             end_time = format_srt_time(segment['end'])
             text = segment['text']
             file.write(f'{i + 1}\n')
             file.write(f'{start_time} --> {end_time}\n')
-            file.write(f'{text}\n\n')
+            if rtl:
+                text = text.strip()
+                file.write(f'\u202b{text}\u202c\n\n')
+            else:
+                file.write(f'{text}\n\n')
 
 def create_subtitles_video(video_path, audio_path, srt_path, output_path):
     # remove the output file if it already exists
@@ -69,49 +77,89 @@ def create_subtitles_video(video_path, audio_path, srt_path, output_path):
     if os.path.exists(temp_file_path):
         os.remove(temp_file_path)
 
-    # remove the audio file
-    if os.path.exists(audio_path):
-        os.remove(audio_path)
-
-
-def transcribe(audio_path, whisper_model_size):
-    logging.info(f'Loading whisper model {whisper_model_size}...')
-    model = whisper.load_model(whisper_model_size).to('cuda')
-    logging.info('Loaded whisper model!')
-    result = model.transcribe(audio_path, fp16=False)
+def transcribe(audio_path):
+    result = MODEL.transcribe(audio_path, fp16=False)
     return result['segments']
 
 def translate(segments, target_language):
+    translated_segments = []
     for segment in segments:
-        segment['text'] = GoogleTranslator(source='auto', target=target_language).translate(segment['text'])
-    return segments
+        translated_text = GoogleTranslator(source='auto', target=target_language).translate(segment['text'])
+        translated_segment = {
+            'start': segment['start'],
+            'end': segment['end'],
+            'text': translated_text
+        }
+        translated_segments.append(translated_segment)
+    return translated_segments
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+
+def process_video(video_path):
+    # get video name
+    video_file_name = video_path.split('/')[-1]
+    video_name = video_file_name.split('.')[0]
+    video_output_directory = os.path.join(OUTPUT_PATH, video_name)
+    if not os.path.exists(video_output_directory):
+        os.makedirs(video_output_directory)
+        
+    video_path = os.path.join(VIDEOS_PATH, video_file_name)
+    audio_path = os.path.join(video_output_directory, f'{video_name}.wav')
+    # iw is Hebrew
+    iw_srt_path = os.path.join(video_output_directory, f'iw_{video_name}.srt')
 
     start_time = time()
     logging.info('Extracting audio from video...')
-    extract_audio_from_video(VIDEO_PATH, AUDIO_PATH)
+    extract_audio_from_video(video_path, audio_path)
     logging.info(f'Audio extracted in {time() - start_time} seconds')
 
     start_time = time()
     logging.info('Transcribing audio...')
-    segments = transcribe(AUDIO_PATH, WHISPER_MODEL_SIZE)
-    save_as_srt(segments, 'data/original_language.srt')
+    segments = transcribe(audio_path)
     logging.info(f'Audio transcribed in {time() - start_time} seconds')
+    logging.info(f'Saving iw segments as SRT...')
+    save_as_srt(segments, iw_srt_path, rtl=True)
 
+    srt_files_paths = {}
+    
     if TRANSLATE:
+        for language_code in LANGUAGE_CODES:
+            language_srt_path = os.path.join(video_output_directory, f'{language_code}_{video_name}.srt')
+            start_time = time()
+            logging.info(f'Translating segments to {language_code}...')
+            translated_segments = translate(segments, language_code)
+            logging.info(f'Segments transtaled to {language_code} in {time() - start_time} seconds')
+            
+            start_time = time()
+            logging.info(f'Saving {language_code} segments as SRT...')
+            save_as_srt(translated_segments, language_srt_path, rtl=language_code in RTL_LANGUAGES)
+
+            srt_files_paths[language_code] = language_srt_path
+
+    # apply subtitles to video
+    for language_code, srt_path in srt_files_paths.items():
+        final_output_path = os.path.join(video_output_directory, f'{language_code}_{video_name}.mp4')
         start_time = time()
-        logging.info('Translating segments...')
-        segments = translate(segments, TARGET_LANGUAGE)
-        logging.info(f'Segments translated in {time() - start_time} seconds')
+        logging.info(f'Creating subtitles video for {language_code}...')
+        create_subtitles_video(video_path, audio_path, srt_path, final_output_path)
+        logging.info(f'Subtitles video created for {language_code} in {time() - start_time} seconds')
 
-    start_time = time()
-    logging.info('Saving segments as SRT...')
-    save_as_srt(segments, SRT_PATH)
-    logging.info(f'Segments saved as SRT in {time() - start_time} seconds')
+    # create subtitles video for iw
+    if ORIGINAL_SUBTITLES:
+        final_output_path = os.path.join(video_output_directory, f'iw_{video_name}.mp4')
+        start_time = time()
+        logging.info(f'Creating subtitles video for iw...')
+        create_subtitles_video(video_path, audio_path, iw_srt_path, final_output_path)
+        logging.info(f'Subtitles video created for iw in {time() - start_time} seconds')
 
-    start_time = time()
-    logging.info('Creating subtitles video...')
-    create_subtitles_video(VIDEO_PATH, AUDIO_PATH, SRT_PATH, FINAL_OUTPUT_PATH)
-    logging.info(f'Subtitles video created in {time() - start_time} seconds')
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    # list all videos in the videos directory
+    videos_files_paths = os.listdir(VIDEOS_PATH)
+
+    for video_path in videos_files_paths:
+        process_video(video_path)
+
+if __name__ == '__main__':
+    main()
